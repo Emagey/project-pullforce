087aff42889573f99c4c2ef98f724cb7f6b1485a
aesyncio <156935888+aesyncio@users.noreply.github.com>
2024-03-26 15:13:35 +0100

    Update Handler.lua

--- Handler.lua
+++ Handler.lua
@@ -2,4 +2,10 @@ if game.PlaceId == 10828925984 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/aesyncio/project-pullforce/main/games/RobotTycoon.lua', true))()
 elseif game.PlaceId == 14448662003 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/aesyncio/project-pullforce/main/games/CarryAFriend.lua', true))()
+else
+  game:GetService("StarterGui"):SetCore("SendNotification", {
+	  Title = "Game Not Supported",
+	  Text = "Loading universal..."
+  })
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/aesyncio/project-pullforce/main/Universal.lua', true))()
 end

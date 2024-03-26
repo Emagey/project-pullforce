03efe82fda6e8b54a8f0cd95312e630d8b26c0c1
aesyncio <156935888+aesyncio@users.noreply.github.com>
2024-03-26 13:14:16 +0100

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,4 +1,4 @@
 -- very easily bypassed but most of the people here are skids and they're dont know shit
-if game.Players.LocalPlayer.Name == 'imjustaboywhosbored' then
+if game.Players.LocalPlayer.Name == 'imjustaboywhosbored' or game.Players.LocalPlayer.Name == '123456789pesos' then
   game.Players.LocalPlayer:Kick('snap back to reality')
 end

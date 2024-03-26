2d5e3d9591705bf978f369ef697ee1bf582dd50b
aesyncio <156935888+aesyncio@users.noreply.github.com>
2024-03-26 13:08:57 +0100

    Create Blacklist.lua

---
+++ Blacklist.lua
@@ -0,0 +1,4 @@
+-- very easily bypassed but most of the people here are skids and they're dont know shit
+if game.Players.LocalPlayer.Name == 'imjustaboywhosbored' then
+  game.Players.LocalPlayer:Kick('snap back to reality')
+end

14f2b5abd2a40ad63e306df2ac1021b96e9fec09
aesyncio <156935888+aesyncio@users.noreply.github.com>
2024-03-26 13:24:33 +0100

    Create Handler.lua

---
+++ Handler.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 10828925984 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/aesyncio/project-pullforce/main/games/RobotTycoon.lua', true))()
+elseif game.PlaceId == 14448662003 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/aesyncio/project-pullforce/main/games/CarryAFriend.lua', true))()
+end
